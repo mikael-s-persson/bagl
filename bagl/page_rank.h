bac5fd5ef2b5e929013525325e34777fbdc0ab64
Mikael Persson <mikael.s.persson@gmail.com>
2024-10-04 15:10:40 -0400

    Added page rank and planar ordering

---
+++ bagl/page_rank.h
@@ -0,0 +1,161 @@
+// Copyright 2004-5 The Trustees of Indiana University.
+// Copyright 2002 Brad King and Douglas Gregor
+//  Authors: Douglas Gregor
+//           Andrew Lumsdaine
+// Copyright 2024 Mikael Persson - Modernized to C++20
+
+#ifndef BAGL_BAGL_PAGE_RANK_H_
+#define BAGL_BAGL_PAGE_RANK_H_
+
+#include <type_traits>
+#include <vector>
+
+#include "bagl/graph_concepts.h"
+#include "bagl/graph_traits.h"
+#include "bagl/properties.h"
+#include "bagl/property_map.h"
+#include "bagl/subgraph.h"
+
+namespace bagl {
+
+namespace page_rank_detail {
+
+struct iterations_limit {
+  explicit iterations_limit(std::size_t n) : n_(n) {}
+
+  template <typename RankMap, typename Graph>
+  bool operator()(const RankMap& /*unused*/, const Graph& /*unused*/) {
+    return n_-- == 0;
+  }
+
+ private:
+  std::size_t n_;
+};
+
+template <concepts::VertexListGraph Graph, concepts::ReadableVertexPropertyMap<Graph> RankMap,
+          concepts::ReadWriteVertexPropertyMap<Graph> RankMap2>
+void page_rank_step(const Graph& g, RankMap from_rank, RankMap2 to_rank, property_traits_value_t<RankMap> damping) {
+  using RankValue = property_traits_value_t<RankMap>;
+  if constexpr (is_bidirectional_graph_v<Graph>) {
+    for (auto v : vertices(g)) {
+      auto rank = RankValue{0};
+      for (auto e : in_edges(v, g)) {
+        rank += get(from_rank, source(e, g)) / out_degree(source(e, g), g);
+      }
+      put(to_rank, v, (RankValue{1} - damping) + damping * rank);
+    }
+  } else {
+    // Set new rank maps
+    for (auto v : vertices(g)) {
+      put(to_rank, v, rank_type(1 - damping));
+    }
+
+    for (auto u : vertices(g)) {
+      auto u_rank_out = damping * get(from_rank, u) / out_degree(u, g);
+      for (auto v : adjacent_vertices(u, g)) {
+        put(to_rank, v, get(to_rank, v) + u_rank_out);
+      }
+    }
+  }
+}
+}  // namespace page_rank_detail
+
+template <concepts::VertexListGraph Graph, concepts::ReadWriteVertexPropertyMap<Graph> RankMap, typename Done,
+          concepts::ReadWriteVertexPropertyMap<Graph> RankMap2>
+void page_rank(const Graph& g, RankMap rank_map, Done done, property_traits_value_t<RankMap> damping, std::size_t n,
+               RankMap2 rank_map2) {
+  using RankValue = property_traits_value_t<RankMap>;
+
+  auto initial_rank = RankValue{RankValue{1} / n};
+  for (auto v : vertices(g)) {
+    put(rank_map, v, initial_rank);
+  }
+
+  bool to_map_2 = true;
+  while ((to_map_2 && !done(rank_map, g)) || (!to_map_2 && !done(rank_map2, g))) {
+    if (to_map_2) {
+      page_rank_detail::page_rank_step(g, rank_map, rank_map2, damping);
+    } else {
+      page_rank_detail::page_rank_step(g, rank_map2, rank_map, damping);
+    }
+    to_map_2 = !to_map_2;
+  }
+
+  if (!to_map_2) {
+    for (auto v : vertices(g)) {
+      put(rank_map, v, get(rank_map2, v));
+    }
+  }
+}
+
+template <concepts::VertexListGraph Graph, concepts::ReadWriteVertexPropertyMap<Graph> RankMap, typename Done>
+void page_rank(const Graph& g, RankMap rank_map, Done done, property_traits_value_t<RankMap> damping, std::size_t n) {
+  using RankValue = property_traits_value_t<RankMap>;
+  page_rank(g, rank_map, done, damping, n, vector_property_map(num_vertices(g), get(vertex_index, g), RankValue{0}));
+}
+
+template <concepts::VertexListGraph Graph, concepts::ReadWriteVertexPropertyMap<Graph> RankMap, typename Done>
+void page_rank(const Graph& g, RankMap rank_map, Done done, property_traits_value_t<RankMap> damping = 0.85) {
+  page_rank(g, rank_map, done, damping, num_vertices(g));
+}
+
+template <concepts::VertexListGraph Graph, concepts::ReadWriteVertexPropertyMap<Graph> RankMap>
+void page_rank(const Graph& g, RankMap rank_map) {
+  page_rank(g, rank_map, page_rank_detail::iterations_limit(20));
+}
+
+// TBD: this could be _much_ more efficient, using a queue to store
+// the vertices that should be reprocessed and keeping track of which
+// vertices are in the queue with a property map. Baah, this only
+// applies when we have a bidirectional graph.
+template <concepts::VertexListGraph G>
+requires concepts::MutableGraph<G>
+void remove_dangling_links(G& g) {
+  bool removed_something = false;
+  do {
+    removed_something = false;
+
+    auto v_rg = vertices(g);
+    if constexpr (std::is_integral_v<graph_vertex_descriptor_t<G>> &&
+                  std::ranges::random_access_range<decltype(v_rg)>) {
+      // Probably a vector-like storage.
+      // Proceed in reverse.
+      for (auto v : vertices(g) | std::views::reverse) {
+        if (out_degree(v, g) == 0) {
+          clear_vertex(v, g);
+          remove_vertex(v, g);
+          removed_something = true;
+          // next `v` is at a lower index than `v`, so we can continue.
+        }
+      }
+    } else if constexpr (std::input_iterator<graph_vertex_descriptor_t<G>> &&
+                         !std::ranges::random_access_range<decltype(v_rg)>) {
+      // Probably a list/set/map-like storage.
+      for (auto it = v_rg.begin(); it != v_rg.end(); /*increment before removal*/) {
+        auto v = *it++;
+        if (out_degree(v, g) == 0) {
+          clear_vertex(v, g);
+          remove_vertex(v, g);
+          removed_something = true;
+          // `it` already moved beyond `v`, so we can continue.
+        }
+      }
+    } else {
+      // Something else, like a filtered range.
+      // Proceed in reverse in case underlying storage is contiguous.
+      for (auto v : vertices(g) | std::views::reverse) {
+        if (out_degree(v, g) == 0) {
+          clear_vertex(v, g);
+          remove_vertex(v, g);
+          removed_something = true;
+          // We cannot continue to iterate after vertex removal.
+          break;
+        }
+      }
+    }
+  } while (removed_something);
+}
+
+}  // namespace bagl
+
+#endif  // BOOST_GRAPH_PAGE_RANK_HPP
---
+++ bagl/planar_canonical_ordering.h
@@ -0,0 +1,152 @@
+// Copyright (c) Aaron Windsor 2007
+// Copyright 2024 Mikael Persson - Modernized to C++20
+
+#ifndef BAGL_BAGL_PLANAR_CANONICAL_ORDERING_H_
+#define BAGL_BAGL_PLANAR_CANONICAL_ORDERING_H_
+
+#include <iterator>
+#include <list>
+#include <vector>
+
+#include "bagl/graph_concepts.h"
+#include "bagl/graph_traits.h"
+#include "bagl/properties.h"
+#include "bagl/property_map.h"
+#include "bagl/vector_property_map.h"
+
+namespace bagl {
+
+enum class planar_canonical_ordering_state { processed, unprocessed, one_neighbor_processed, ready_to_be_processed, discard };
+
+template <concepts::VertexListGraph Graph, concepts::LvalueVertexPropertyMap<Graph> PlanarEmbedding,
+          std::output_iterator<graph_vertex_descriptor_t<Graph>> OutputIterator,
+          concepts::ReadableVertexIndexMap<Graph> VertexIndexMap>
+requires concepts::AdjacencyGraph<Graph>
+void planar_canonical_ordering(const Graph& g, PlanarEmbedding embedding, OutputIterator ordering, VertexIndexMap vm) {
+  using vertex_t = graph_vertex_descriptor_t<Graph>;
+  using edge_t = graph_edge_descriptor_t<Graph>;
+
+  auto processed_neighbor = vector_property_map(num_vertices(g), vm, graph_traits<Graph>::null_vertex());
+  auto status = vector_property_map(num_vertices(g), vm, planar_canonical_ordering_state::unprocessed);
+
+  std::list<vertex_t> ready_to_be_processed;
+
+  vertex_t first_vertex = *vertices(g).begin();
+  vertex_t second_vertex = first_vertex;
+  for (auto u : adjacent_vertices(first_vertex, g)) {
+    if (u != first_vertex) {
+      second_vertex = u;
+      break;
+    }
+  }
+
+  ready_to_be_processed.push_back(first_vertex);
+  status[first_vertex] = planar_canonical_ordering_state::ready_to_be_processed;
+  ready_to_be_processed.push_back(second_vertex);
+  status[second_vertex] = planar_canonical_ordering_state::ready_to_be_processed;
+
+  while (!ready_to_be_processed.empty()) {
+    vertex_t u = ready_to_be_processed.front();
+    ready_to_be_processed.pop_front();
+
+    if (status[u] != planar_canonical_ordering_state::ready_to_be_processed && u != second_vertex) {
+      continue;
+    }
+
+    auto ei_start = embedding[u].begin();
+    auto ei_end = embedding[u].end();
+    auto prior_edge_itr = std::prev(ei_end);
+    while (source(*prior_edge_itr, g) == target(*prior_edge_itr, g)) {
+      prior_edge_itr = std::prev(prior_edge_itr);
+    }
+
+    for (auto ei = ei_start; ei != ei_end; ++ei) {
+      edge_t e(*ei);  // e = (u,v)
+      auto next_edge_itr = std::next(ei) == ei_end ? ei_start : std::next(ei);
+      vertex_t v = (source(e, g) == u ? target(e, g) : source(e, g));
+
+      vertex_t prior_vertex =
+          (source(*prior_edge_itr, g) == u ? target(*prior_edge_itr, g) : source(*prior_edge_itr, g));
+      vertex_t next_vertex = (source(*next_edge_itr, g) == u ? target(*next_edge_itr, g) : source(*next_edge_itr, g));
+
+      // Need prior_vertex, u, v, and next_vertex to all be
+      // distinct. This is possible, since the input graph is
+      // triangulated. It'll be true all the time in a simple
+      // graph, but loops and parallel edges cause some complications.
+      if (prior_vertex == v || prior_vertex == u) {
+        prior_edge_itr = ei;
+        continue;
+      }
+
+      // Skip any self-loops
+      if (u == v) {
+        continue;
+      }
+
+      // Move next_edge_itr (and next_vertex) forwards
+      // past any loops or parallel edges
+      while (next_vertex == v || next_vertex == u) {
+        next_edge_itr = (std::next(next_edge_itr) == ei_end ? ei_start : std::next(next_edge_itr));
+        next_vertex = (source(*next_edge_itr, g) == u ? target(*next_edge_itr, g) : source(*next_edge_itr, g));
+      }
+
+      if (status[v] == planar_canonical_ordering_state::unprocessed) {
+        status[v] = planar_canonical_ordering_state::one_neighbor_processed;
+        processed_neighbor[v] = u;
+      } else if (status[v] == planar_canonical_ordering_state::one_neighbor_processed) {
+        vertex_t x = processed_neighbor[v];
+        // are edges (v,u) and (v,x) adjacent in the planar
+        // embedding? if so, set status[v] = 1. otherwise, set
+        // status[v] = 2.
+
+        if ((next_vertex == x && !(first_vertex == u && second_vertex == x)) ||
+            (prior_vertex == x && !(first_vertex == x && second_vertex == u))) {
+          status[v] = planar_canonical_ordering_state::ready_to_be_processed;
+        } else {
+          status[v] = planar_canonical_ordering_state::discard;
+        }
+      } else if (status[v] > planar_canonical_ordering_state::one_neighbor_processed) {
+        // check the two edges before and after (v,u) in the planar
+        // embedding, and update status[v] accordingly
+
+        bool processed_before = false;
+        if (status[prior_vertex] == planar_canonical_ordering_state::processed) {
+          processed_before = true;
+        }
+
+        bool processed_after = false;
+        if (status[next_vertex] == planar_canonical_ordering_state::processed) {
+          processed_after = true;
+        }
+
+        if (!processed_before && !processed_after) {
+          ++status[v];
+
+        } else if (processed_before && processed_after) {
+          --status[v];
+        }
+      }
+
+      if (status[v] == planar_canonical_ordering_state::ready_to_be_processed) {
+        ready_to_be_processed.push_back(v);
+      }
+
+      prior_edge_itr = ei;
+    }
+
+    status[u] = planar_canonical_ordering_state::processed;
+    *ordering = u;
+    ++ordering;
+  }
+}
+
+template <concepts::VertexListGraph Graph, concepts::LvalueVertexPropertyMap<Graph> PlanarEmbedding,
+          std::output_iterator<graph_vertex_descriptor_t<Graph>> OutputIterator>
+requires concepts::AdjacencyGraph<Graph>
+void planar_canonical_ordering(const Graph& g, PlanarEmbedding embedding, OutputIterator ordering) {
+  planar_canonical_ordering(g, embedding, ordering, get(vertex_index, g));
+}
+
+}  // namespace bagl
+
+#endif  // BAGL_BAGL_PLANAR_CANONICAL_ORDERING_H_
