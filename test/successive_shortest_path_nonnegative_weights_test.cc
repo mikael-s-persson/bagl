701638a1b6c1d45d0d5c4769b0ef76edde3fc8bf
Mikael Persson <mikael.s.persson@gmail.com>
2024-11-09 13:17:22 -0500

    Added bundled props to find flow test

--- test/successive_shortest_path_nonnegative_weights_test.cc
+++ test/successive_shortest_path_nonnegative_weights_test.cc
@@ -4,7 +4,9 @@
 #include "bagl/successive_shortest_path_nonnegative_weights.h"
 
 #include "bagl/find_flow_cost.h"
+#include "bagl/graph_traits.h"
 #include "bagl/properties.h"
+#include "bagl/property_map.h"
 #include "test/min_cost_max_flow_utils.h"
 
 namespace bagl {
@@ -44,6 +46,38 @@ TEST(SuccShortestPathAugmentation, Test2) {
 
   int cost = find_flow_cost(g);
   EXPECT_EQ(cost, 7);
+}
+
+struct TestEdgeProp {
+  int capacity;
+  int residual_capacity;
+  int cost;
+  std::size_t id;  // unused
+};
+struct TestNodeProp {
+  int dist;
+  int dist_prev;
+};
+
+TEST(SuccShortestPathAugmentation, Bundled) {
+  using Graph = adjacency_list<list_s, vec_s, directed_s, TestNodeProp, TestEdgeProp>;
+  using Edge = graph_edge_descriptor_t<Graph>;
+  Graph g;
+  std::unordered_map<Edge, Edge, graph_descriptor_hash_t<Edge>> rev_cont;
+  auto rev = associative_property_map(rev_cont);
+  auto [s, t] = sample_graph::get_sample_graph(g, rev);
+
+  int N = num_vertices(g);
+  auto idx = get(vertex_index, g);
+  auto edge_pred = vector_property_map(N, idx, Edge{});
+
+  successive_shortest_path_nonnegative_weights(
+      g, s, t, get(&TestEdgeProp::capacity, g), get(&TestEdgeProp::residual_capacity, g), get(&TestEdgeProp::cost, g),
+      rev, idx, edge_pred.ref(), get(&TestNodeProp::dist, g), get(&TestNodeProp::dist_prev, g));
+
+  int cost = find_flow_cost(g, get(&TestEdgeProp::capacity, g), get(&TestEdgeProp::residual_capacity, g),
+                            get(&TestEdgeProp::cost, g));
+  EXPECT_EQ(cost, 29);
 }
 
 }  // namespace
